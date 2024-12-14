625f0283571e34700ab17db36bf24e0c11ce0008
Shubham Sharma <samshubham142@gmail.com>
2024-12-14 12:12:54 +0530

    File setup done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-ÿþ
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const path = require("path");
+const env = require("dotenv").config();
+const connectDB = require("./connectDB");
+
+// set view engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+// DB-CONNECTION
+connectDB(process.env.DB_URL)
+  .then(() => {
+    console.log("DB-CONNECTED");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.get("/", (req, res) => {
+  res.render("home");
+});
+// server listening on PORT
+app.listen(process.env.PORT, () => {
+  console.log(`server listening ON PORT ${process.env.PORT} `);
+});
